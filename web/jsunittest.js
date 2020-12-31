9fdeb2d9a0ea0bf8b65e3407ac4eadd5c48bb9c1
晓风轻 <121509092@qq.com>
2021-01-01 01:17:24 +0800

    引入mocha框架

---
+++ web/jsunittest.js
@@ -0,0 +1,21 @@
+// assert: nodejs内置断言模块
+var assert  = require("assert");
+
+const add = function(a,b){return a+b};
+
+// describe：定义一组测试
+describe("加法函数测试", function() {
+    before(function() {
+        // runs before all tests in this block
+    });
+    
+    // it: 定义一个测试用例
+    it("1 加 1 应该等于 2", function() {
+
+        assert.equal(add(1, 1), 2);
+    });
+    
+    after(function() {
+        // runs after all test in this block
+    });
+});
